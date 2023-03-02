58a3e135b3c7c52bb02ce08f4447cbfab7e7f5c7
Ivan <maverickmsk1@gmail.com>
2023-03-02 23:37:16 +0300

    basic integration tests added

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,3 +1,17 @@
+import pytest
+
+from conftest import probe_names
+
+
 class TestIntegration:
     def test_startup(self, mock_client):
         assert mock_client.get("/").status_code == 200
+        assert mock_client.get("/metrics").status_code == 200
+        assert mock_client.get("/docs").status_code == 200
+        assert mock_client.get("/health/liveness").status_code == 200
+        assert mock_client.get("/health/readiness").status_code == 200
+
+    @pytest.mark.parametrize("req", probe_names)
+    def test_probes(self, mock_client, req):
+        probe = mock_client.get(f"/health/{req}")
+        assert probe.json() == {req: True}
